3a0e3dd87754605ecd0fd45d1923c0c5919fb1df
Parker Boeing <parker.boeing@gmail.com>
2023-09-25 16:02:21 -0600

    test: User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+
+  describe "validations" do
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:password)}
+    it { should validate_confirmation_of(:password) }
+
+    it "properly stores passwords" do
+      user = User.create!(email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
+  end
+end
