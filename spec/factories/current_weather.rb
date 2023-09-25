bdbd2a70bb36f299e9c5f40708e3a039b1f0cde7
Parker Boeing <parker.boeing@gmail.com>
2023-09-24 19:31:45 -0600

    test: Locations and forecasts facades

--- spec/factories/current_weather.rb
+++ spec/factories/current_weather.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     initialize_with { new(attributes) }
     last_updated { Faker::Time.backward(days: 30, period: :evening).strftime("%Y-%m-%d %H:%M") }
     temperature { Faker::Number.between(from: 20.0, to: 100.0).round(1) }
-    feels_like { Faker::Number.between(from: 20.0, to: 100.0).round(1) }
+    feels_like { 90 }
     humidity { Faker::Number.between(from: 0, to: 100) }
     uvi { Faker::Number.decimal(l_digits: 1).to_f }
     visibility { Faker::Number.between(from: 0, to: 10) }
