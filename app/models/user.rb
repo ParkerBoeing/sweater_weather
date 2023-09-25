63ef7ae06c504afce988ff04857b562c17f739d6
Parker Boeing <parker.boeing@gmail.com>
2023-09-25 16:29:27 -0600

    test: Update user model to check for api_key functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password, confirmation: true
+  validates :api_key, uniqueness: true
+  before_create :generate_api_key
   has_secure_password
+
+  private
+  
+  def generate_api_key
+    loop do
+      self.api_key = SecureRandom.hex(20)
+      break unless User.exists?(api_key: api_key)
+    end
+  end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
+    it {should validate_uniqueness_of(:api_key)}
     it {should validate_presence_of(:password)}
     it { should validate_confirmation_of(:password) }
 
@@ -12,6 +13,11 @@ RSpec.describe User, type: :model do
       user = User.create!(email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
       expect(user).to_not have_attribute(:password)
       expect(user.password_digest).to_not eq('password123')
+    end
+
+    it "generates an API key upon creation" do
+      user = User.create!(email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user.api_key.length).to eq(40)
     end
   end
 end
