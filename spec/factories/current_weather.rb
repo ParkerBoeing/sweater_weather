2232c1ec352abe8deb8d768f67337edec2875975
Parker Boeing <parker.boeing@gmail.com>
2023-09-24 19:12:55 -0600

    test: All remaining POROs

--- spec/factories/current_weather.rb
+++ spec/factories/current_weather.rb
@@ -4,10 +4,14 @@ FactoryBot.define do
     last_updated { Faker::Time.backward(days: 30, period: :evening).strftime("%Y-%m-%d %H:%M") }
     temperature { Faker::Number.between(from: 20.0, to: 100.0).round(1) }
     feels_like { Faker::Number.between(from: 20.0, to: 100.0).round(1) }
-    icon { Faker::LoremPixel.image(category: 'nature') } 
     humidity { Faker::Number.between(from: 0, to: 100) }
     uvi { Faker::Number.decimal(l_digits: 1).to_f }
     visibility { Faker::Number.between(from: 0, to: 10) }
-    condition { Faker::Weather.condition }
+    condition do
+      {
+        icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
+        text: Faker::Lorem.word
+      }
+    end
   end
 end
--- spec/factories/hourly_weather.rb
+++ spec/factories/hourly_weather.rb
@@ -3,7 +3,11 @@ FactoryBot.define do
     initialize_with { new(attributes) }
     time { Faker::Time.forward(days: 2, period: :all).strftime("%I:%M %p") }
     temperature { Faker::Number.between(from: 20.0, to: 100.0).round(1) }
-    conditions { Faker::Weather.condition }
-    icon { Faker::LoremPixel.image(category: 'nature') }
+    condition do
+      {
+        icon: "//cdn.weatherapi.com/weather/64x64/night/116.png",
+        text: Faker::Lorem.word
+      }
+    end
   end
 end
