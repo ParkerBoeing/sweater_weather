31a667ae7802e3def39a498236450f2de5f65c10
Parker Boeing <parker.boeing@gmail.com>
2023-09-25 09:38:36 -0600

    feat: Books facade returns hash with proper data

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -2,8 +2,14 @@ class BooksFacade
   def self.book_search(location, quantity)
     service = LibraryService.new
     raw_books = service.search_by_location_name(location, quantity)
-    raw_books[:docs].map do |book|
-      Book.new(book)
-    end
+    books = raw_books[:docs].map do |book|
+              Book.new(book)
+            end
+    data = {
+            books: books,
+            total_books_found: raw_books[:numFound]
+          }
+
+    data
   end
 end
