791bbfe6c88f6e89768ac68a5bf71ec2a6496a43
Parker Boeing <parker.boeing@gmail.com>
2023-09-26 10:49:00 -0600

    test: Happy and sad paths for user login post request

--- spec/requests/api/v0/post_session_create_spec.rb
+++ spec/requests/api/v0/post_session_create_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe "Sessions Requests", type: :request do
     end
 
     describe "sad path", :vcr do
-      it "returns an error message for duplicate user" do
+      it "returns an error for invalid credentials" do
         user_params = {
           email: "whatever2@example.com",
           password: "password123",
@@ -40,39 +40,20 @@ RSpec.describe "Sessions Requests", type: :request do
         }
         post "/api/v0/users", params: user_params
         expect(response).to have_http_status(:success)
-        post "/api/v0/users", params: user_params
-        expect(response.status).to eq(400)
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response).to eq({"error"=>"Validation failed: Email has already been taken"})     
-      end
 
-      it "returns an error message if password and confirmation dont match" do
         user_params = {
-          email: "whatever@example.com",
-          password: "password",
-          password_confirmation: "password123"
+          email: "whateve@example.com",
+          password: "password123"
         }
-        post "/api/v0/users", params: user_params
-        expect(response.status).to eq(400)
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response).to eq({"error"=>"Validation failed: Password confirmation doesn't match Password"})
-      end
-
-      it "returns an error for missing parameters" do
-        user_params = {
-          email: "whatever@example.com",
-          password: ""
-        }
-        post "/api/v0/users", params: user_params
-        expect(response.status).to eq(400)
+        post "/api/v0/sessions", params: user_params
         parsed_response = JSON.parse(response.body)
         expect(parsed_response).to eq({"error"=>"Invalid credentials"})
         
         user_params = {
-          email: "",
-          password: "password123"
+          email: "whatever2@example.com",
+          password: "passwor"
         }
-        post "/api/v0/users", params: user_params
+        post "/api/v0/sessions", params: user_params
         parsed_response = JSON.parse(response.body)
         expect(parsed_response).to eq({"error"=>"Invalid credentials"})
       end
