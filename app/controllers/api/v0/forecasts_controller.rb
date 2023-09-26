7d268a2ce555647c4943f5db265e60690ac3bb7a
Parker Boeing <parker.boeing@gmail.com>
2023-09-26 13:05:03 -0600

    feat: Get_road_trip service instance method

--- app/controllers/api/v0/forecasts_controller.rb
+++ app/controllers/api/v0/forecasts_controller.rb
@@ -5,4 +5,14 @@ class Api::V0::ForecastsController < ApplicationController
     forecast = ForecastsFacade.get_forecast(location)
     render json: ForecastSerializer.new(forecast).as_json
   end
+  
+  def road_trip
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      response = LocationService.new.get_road_trip(params[:origin], params[:destination])
+      require 'pry'; binding.pry
+    else
+      render json: { error: "Unauthorized" }, status: 401
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "api/v0/forecast", to: "api/v0/forecasts#show"
   post "api/v0/users", to: "api/v0/users#create"
   post "api/v0/sessions", to: "api/v0/sessions#create"
+  post "api/v0/road_trip", to: "api/v0/forecasts#road_trip"
 end
